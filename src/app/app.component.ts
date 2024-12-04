d22a1ed1399db624122a6672ad727808dd36b090
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-12-04 15:53:18 +0100

    delete boiler plate app and add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  template: `
+    <header>
+      <img
+        src="assets/angular-logo.png"
+        alt="The Angular logo: The letter 'A'"
+      />
+      <h1>Let's get started!</h1>
+      <p>Time to learn all about Angular!</p>
+    </header>
+  `,
+  styles: `
+    header {
+      margin: 3rem auto;
+      text-align: center;
+    }
+
+    img {
+      width: 8rem;
+    }
+
+    h1 {
+      margin: 1rem auto;
+      background: -webkit-linear-gradient(45deg, #f9096d, #b700ff);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      font-size: 3rem;
+    }
+  `,
 })
 export class AppComponent {}
